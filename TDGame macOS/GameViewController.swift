e9e7a17835938fa181b6f1c6b3a394ffd4b00a4f
wttch（王冲） <wttch@wttch.com>
2023-06-28 00:30:40 +0800

    Initial Commit

---
+++ TDGame macOS/GameViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GameViewController.swift
+//  TDGame macOS
+//
+//  Created by Wttch on 2023/6/28.
+//
+
+import Cocoa
+import SpriteKit
+import GameplayKit
+
+class GameViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let scene = GameScene.newGameScene()
+        
+        // Present the scene
+        let skView = self.view as! SKView
+        skView.presentScene(scene)
+        
+        skView.ignoresSiblingOrder = true
+        
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+    }
+
+}
+
